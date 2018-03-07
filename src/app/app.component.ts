871bbb5157e828926d931318a13135acc4acf593
ArabicHTML <aeldewiny@gmail.com>
2018-03-07 20:15:04 +0100

    convert currency app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth-service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public show: boolean;
+  constructor(public auth: AuthService, private router: Router) {
+    this.checkShow(this.show);
+  }
+
+  // show and hide nav buttons
+  checkShow(value) {
+    this.auth.authChangeObs()
+      .subscribe((res) => {
+        value = res;
+        if (this.show) {
+          this.show = value;
+        } else {
+          this.show = value;
+        }
+      })
+  }
+
+  logout() {
+    this.auth.logout();
+    this.router.navigate(['login']);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AuthService } from './services/auth-service/auth.service';
+import { CurrencyService } from './services/currency-service/currency.service';
 
 
 import { AppComponent } from './app.component';
+import { ConvertComponent } from './convert/convert.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
+const appRoutes: Routes =
+  [
+    {
+      path: '',
+      component: HomeComponent
+    }, {
+      path: 'register',
+      component: RegisterComponent
+    }, {
+      path: 'login',
+      component: LoginComponent
+    }, {
+      path: 'convert',
+      component: ConvertComponent
+    }
+  ];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConvertComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    routing
   ],
-  providers: [],
+  providers: [AuthService, CurrencyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
